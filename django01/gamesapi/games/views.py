31d17e876fca971b2927882d642d1813f138903b
hxperl <hxperl@gmail.com>
2017-11-07 01:29:11 +0900

    Update : views.py

--- django01/gamesapi/games/views.py
+++ django01/gamesapi/games/views.py
@@ -1,70 +1,51 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
-from rest_framework import status
+from games.models import GameCategory
 from games.models import Game
+from games.models import Player
+from games.models import PlayerScore
+from games.serializers import GameCategorySerializer
 from games.serializers import GameSerializer
+from games.serializers import PlayerSerializer
+from games.serializers import PlayerScoreSerializer
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.response import reverse
 
-# Create your views here.
+class GameCategoryList(generics.ListCreateAPIView):
+    queryset = GameCategory.objects.all()
+    serializer_class = GameCategorySerializer
+    name = 'gamecategory-list'
 
-class JSONResponse(HttpResponse):
-    """
-    HttpResponse 클래스는 내용이 문자열로 된 HTTP 응답을 리턴한다.
-    JSONResponse 클래스는 HttpResponse 클래스를 상속받아 구현했고 내용을 JSON으로 렌더링한다.
-    """
+class GameCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = GameCategory.objects.all()
+    serializer_class = GameCategorySerializer
+    name = 'gamecategory-detail'
 
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
+class GameList(generics.ListCreateAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    name = 'game-list'
 
+class GameDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    name = 'game-detail'
 
-@api_view(['GET', 'POST'])
-def game_list(request):
-    """
-    모든 게임을 나열하거나 새 게임을 생성한다.
-    request 인자로 HttpRequest 인스턴스를 받는다.
-    GET, POST 두 가지 동사를 처리할 수 있다.
-    """
+class PlayerList(generics.ListCreateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    name = 'player-list'
 
-    if request.method == 'GET':
-        games = Game.objects.all()
-        games_serializer = GameSerializer(games, many=True)
-        return JSONResponse(games_serializer.data)
+class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    name = 'player-detail'
 
-    elif request.method == 'POST':
-        games_serializer = GameSerializer(data=request.data)
-        if games_serializer.is_valid():
-            games_serializer.save()
-            return Response(games_serializer.data, status=status.HTTP_201_CREATED)
-        return Response(games_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PlayerScoreList(generics.ListCreateAPIView):
+    queryset = PlayerScore.objects.all()
+    serializer_class = PlayerScoreSerializer
+    name = 'playerscore-list'
 
-@api_view(['GET', 'PUT', 'POST'])
-def game_detail(request, pk):
-    """
-    기존의 게임을 검색, 업데이트, 삭제한다.
-    request 인자로 HttpRequest 인스턴스를 받고 pk 인자로는 검색, 업데이트, 삭제할 게임의 기본 키 또는 식별자를 받는다.
-    """
-
-    try:
-        game = Game.objects.get(pk=pk)
-    except Game.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        game_serializer = GameSerializer(game)
-        return Response(game_serializer.data)
-
-    elif request.method == 'PUT':
-        game_serializer = GameSerializer(game, data = request.data)
-        if game_serializer.is_valid():
-            game_serializer.save()
-            return Response(game_serializer.data)
-        return Response(game_serializer.errors, status=status.HTTP_404_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        game.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class PlayerScoreDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PlayerScore.objects.all()
+    serializer_class = PlayerScoreSerializer
+    name = 'playerscore-detail'

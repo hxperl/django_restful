c9d0fa48c275fe5ec7f4e0a37b20eea02e082ad0
hxperl <hxperl@gmail.com>
2017-11-06 14:58:28 +0900

    Update : view.py - apply api_view decorator

--- django01/gamesapi/games/views.py
+++ django01/gamesapi/games/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
+from django.views.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 from games.models import Game
@@ -21,7 +21,7 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
-@csrf_exempt    # 데코레이터를 적용해 뷰가 사이트 간 요청 위조 쿠키를 설정하게 한다.
+@api_view(['GET', 'POST'])
 def game_list(request):
     """
     모든 게임을 나열하거나 새 게임을 생성한다.
@@ -35,14 +35,13 @@ def game_list(request):
         return JSONResponse(games_serializer.data)
 
     elif request.method == 'POST':
-        game_data = JSONParser().parse(request)
-        games_serializer = GameSerializer(data=game_data)
+        games_serializer = GameSerializer(data=request.data)
         if games_serializer.is_valid():
             games_serializer.save()
-            return JSONResponse(games_serializer.data, status=status.HTTP_201_CREATED)
-        return JSONResponse(games_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(games_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(games_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'POST'])
 def game_detail(request, pk):
     """
     기존의 게임을 검색, 업데이트, 삭제한다.
@@ -52,20 +51,19 @@ def game_detail(request, pk):
     try:
         game = Game.objects.get(pk=pk)
     except Game.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         game_serializer = GameSerializer(game)
-        return JSONResponse(game_serializer.data)
+        return Response(game_serializer.data)
 
     elif request.method == 'PUT':
-        game_data = JSONParser().parse(request)
-        game_serializer = GameSerializer(game, data = game_data)
+        game_serializer = GameSerializer(game, data = request.data)
         if game_serializer.is_valid():
             game_serializer.save()
-            return JSONResponse(game_serializer.data)
-        return JSONResponse(game_serializer.errors, status=status.HTTP_404_BAD_REQUEST)
+            return Response(game_serializer.data)
+        return Response(game_serializer.errors, status=status.HTTP_404_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         game.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)

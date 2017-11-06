7bf3837c709b9176c133a9921a8bc741dedb1fed
hxperl <hxperl@gmail.com>
2017-11-06 15:00:12 +0900

    Update : fix import module error

--- django01/gamesapi/games/views.py
+++ django01/gamesapi/games/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators import api_view
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework import status
